3be7fda1f7ab42ccab1e7558bc99df2167bf0bc7
Dev-Fady <fadymobiledev@gmail.com>
2025-09-22 18:29:56 +0300

    add ALL Dbset

--- Restaurant-Chain-Management/Models/AppDbContext.cs
+++ Restaurant-Chain-Management/Models/AppDbContext.cs
@@ -8,6 +8,15 @@ namespace Restaurant_Chain_Management.Models
         public DbSet<City> Cities { get; set; }
         public DbSet<Branch> Branches { get; set; }
         public DbSet<Employee> Employees { get; set; }
+       
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ImageProduct> ImageProducts { get; set; }
+        public DbSet<FavoriteProduct> FavoriteProducts { get; set; }
+
+        public DbSet<StockProduct> StockProducts { get; set; }
+
+
 
         public AppDbContext() : base()
         { 
