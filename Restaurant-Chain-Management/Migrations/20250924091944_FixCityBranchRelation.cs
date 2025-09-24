14ba4c433d5ee8517dd918c0c4dfcd4d931c9ffb
Dev-Fady <fadymobiledev@gmail.com>
2025-09-24 15:50:27 +0300

    Some Test

---
+++ Restaurant-Chain-Management/Migrations/20250924091944_FixCityBranchRelation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant_Chain_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCityBranchRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityId1",
+                table: "Branches",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Branches_CityId1",
+                table: "Branches",
+                column: "CityId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Branches_Cities_CityId1",
+                table: "Branches",
+                column: "CityId1",
+                principalTable: "Cities",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Branches_Cities_CityId1",
+                table: "Branches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Branches_CityId1",
+                table: "Branches");
+
+            migrationBuilder.DropColumn(
+                name: "CityId1",
+                table: "Branches");
+        }
+    }
+}
